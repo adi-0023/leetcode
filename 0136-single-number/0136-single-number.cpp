93af1fe171bf8c2f906f5a5d2bdd2e78e4af90e8
Aditya Sharma <105793200+adi-0023@users.noreply.github.com>
2024-01-14 11:52:40 +0530

    Time: 11 ms (89.79%), Space: 17.1 MB (99.19%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int num= 0;
+        for (int i = 0; i<nums.size(); i++){
+            // int num = 0;
+            num = num ^ nums[i];
+        }
+        return num;
+        
+    }
+};
